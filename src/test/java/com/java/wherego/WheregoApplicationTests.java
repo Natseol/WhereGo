b8eff5e1cfc87b246258f7ef8b2cca551885e5c6
Natseol <jewelbox00@gmail.com>
2023-12-05 09:50:30 +0900

    feat : first commit

---
+++ src/test/java/com/java/wherego/WheregoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.wherego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheregoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
