5a682a3fd6a6b886b634298410c02b0008a6aa01
Natseol <jewelbox00@gmail.com>
2023-12-06 17:59:57 +0900

    fix : css

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -1,7 +1,7 @@
 const registButton = document.getElementById("index-regist-button");
 registButton.onclick = (e) => {
     document.getElementById("index-mid-form-login").style.display="none";
-    document.getElementById("index-mid-form-regist").style.display="block";
+    document.getElementById("index-mid-form-regist").style.display="block";    
     document.body.style = "background-color: rgba( 0 0, 0, 0.7 )";
 };
 
